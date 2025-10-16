ae9dd17f67f386f60574072519e5a71d4359ee95
yashchavan <yash.s.chavan3@gmail.com>
2025-10-16 17:54:05 -0500

    Initial Commit

---
+++ AI School Event AppTests/AI_School_Event_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_School_Event_AppTests.swift
+//  AI School Event AppTests
+//
+//  Created by Yash Chavan on 10/16/25.
+//
+
+import Testing
+@testable import AI_School_Event_App
+
+struct AI_School_Event_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
